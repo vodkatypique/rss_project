d54c07abf20c2e14fe2db3fcd7724e4f9c2d928c
Vodkatypique <38146817+vodkatypique@users.noreply.github.com>
2022-05-22 22:00:53 +0200

    delete route

--- backend/backend_api/urls.py
+++ backend/backend_api/urls.py
@@ -4,5 +4,4 @@ from backend_api import views
 router = DefaultRouter()
 router.register(r'feeds', views.FeedViewSet, basename='feed')
 router.register(r'items', views.ItemViewSet, basename='item')
-router.register(r'update', views.ItemViewSet, basename='update')
 urlpatterns = router.urls
